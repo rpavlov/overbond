80156af34e40976177519621939b553717d200f3
rpavlov <roumen@rpavlov.com>
2017-11-16 12:58:03 -0500

    initial commit

---
+++ lib/spread.rb
@@ -0,0 +1,14 @@
+
+class Spread
+  def self.calculate(args)
+    p corporate(args)
+    p government(args)
+  end
+
+  def self.corporate(args)
+    args
+  end
+  def self.government(args)
+    args
+  end
+end
