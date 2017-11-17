afdfd0c0a9f99c2f6d1b25f8850cd7e1db6a53e7
rpavlov <roumen@rpavlov.com>
2017-11-16 21:37:12 -0500

    Finish algorithms

--- lib/spread.rb
+++ lib/spread.rb
@@ -1,10 +1,12 @@
 require 'csv'
-require "bigdecimal"
+require 'bigdecimal'
 require 'bigdecimal/util'
 require 'byebug'
+
 class Spread
   def self.calculate(args)
-    data = self.parse_data(args[:input])
+    data = parse_data(args[:input])
+
     if args[:benchmark]
       puts benchmark(data).map{|row|row.join(",")}
     end
@@ -24,12 +26,11 @@ class Spread
 
   def self.benchmark(data)
     result = [["bond", "benchmark", "spread_to_benchmark"]]
-    government_bonds = data.select{|bond| bond["type"] == "government"}
 
     data.select{|bond| bond["type"] == "corporate"}.each do |bond|
-      benchmark = self.best_benchmark_for(bond, government_bonds)
+      benchmark = best_benchmark_for(bond, data.select{|bond| bond["type"] == "government"})
 
-      spread = (bond["yield"].to_d - benchmark["yield"].to_d).to_digits #Is it possible for gov bond yield to exceed corp. yield?
+      spread = (bond["yield"].to_d - benchmark["yield"].to_d).abs.to_digits #Do we consider negative yield?
       result << [bond["bond"], benchmark["bond"], spread + "%"]
     end
     result
@@ -37,16 +38,41 @@ class Spread
 
   def self.curve(data)
     result = [["bond", "spread_to_curve"]]
+    government_bonds = data.select{|bond| bond["type"] == "government"}
+
+    data.select{|bond| bond["type"] == "corporate"}.each do |bond|
+
+      spread = bond["yield"].to_d - linear_interpolation(bond, government_bonds).round(2)
+      result << [bond["bond"], spread.to_digits + "%"]
+    end
+    result
   end
 
   def self.best_benchmark_for(corporate_bond, government_bonds)
-    closest = nil
+    best_bonds = best_benchmarks_for_curve(corporate_bond, government_bonds).reject(&:nil?)
 
-    government_bonds.each do |bond|
-      if closest.nil? || (bond["term"].to_d - corporate_bond["term"].to_d).abs < (closest["term"].to_d - corporate_bond["term"].to_d).abs
-        closest = bond #TODO: In the case of two bonds with the same time, we probably want the one with the greater yield.
-      end
+    #All bond terms are either before or after, not on either side (and one was therefore nil and excluded).
+    if best_bonds.size == 1
+      best_bonds[0]
+    else
+      before = (best_bonds[0]["term"].to_d - corporate_bond["term"].to_d).abs
+      after =  (best_bonds[1]["term"].to_d - corporate_bond["term"].to_d).abs
+      before < after ? best_bonds[0] : best_bonds[1]
     end
-    closest
+  end
+
+  #Here we want to find two closest bonds, with terms closest before and after that of the corporate bond.
+  def self.best_benchmarks_for_curve(corporate_bond, government_bonds)
+    combined_bonds = (government_bonds << corporate_bond).sort_by{|bond| bond["term"].to_d}
+
+    [combined_bonds[combined_bonds.index(corporate_bond) - 1], combined_bonds[combined_bonds.index(corporate_bond) + 1]]
+  end
+
+  def self.linear_interpolation(corporate_bond, government_bonds)
+    bonds = best_benchmarks_for_curve(corporate_bond, government_bonds)
+
+      ( bonds[0]["yield"].to_d * (bonds[1]["term"].to_d - corporate_bond["term"].to_d) +
+      bonds[1]["yield"].to_d * ( corporate_bond["term"].to_d - bonds[0]["term"].to_d ) ) /
+      (bonds[1]["term"].to_d - bonds[0]["term"].to_d)
   end
 end
